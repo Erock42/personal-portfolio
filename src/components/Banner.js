adba1811599b4adab31dbb47c0079afcd80dff58
Erock FaQue <eric.kemerer@gmail.com>
2023-05-10 21:11:03 -0400

    banner CSS, need sticky pic and address jump on text animation; take out extra button

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,8 +57,8 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my portfolio</span>
-                        <h1>{"Hi I'm Eric! "} <span className="wrap">{text}</span></h1>
+                        <span className="tagline">Hello, I'm Eric!</span>
+                        <h1>{"I'm a "} <span className="wrap">{text}</span></h1>
                         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
                         <button onClick={() => console.log('connect')}>"Let's Connect!"<ArrowRightCircle size={25}/></button>
                     </Col>
