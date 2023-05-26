ff81562d2d55f5af8851b4524f70b33d1e5f4cda
Erock FaQue <eric.kemerer@gmail.com>
2023-05-26 19:27:01 -0400

    add rest of css to new projects.js and card components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
@@ -34,8 +35,10 @@ class App extends Component {
           <NavBar />
           <Banner />
           <Skills />
+          <Projects />
         </div>
       );
     }
-  }
+}
+  
 export default App;
