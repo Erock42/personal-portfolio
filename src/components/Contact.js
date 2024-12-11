397558c091299fd6853f440ac189c9e0c7aec608
Erock42 <95666323+Erock42@users.noreply.github.com>
2024-12-10 22:10:53 -0500

    took out phone requirment in form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,7 +39,7 @@ export const Contact = () => {
                 templateParams,
                 process.env.REACT_APP_EMAILJS_PUBLIC_KEY
             );
-            
+
             setButtonText("Send");
             setFormDetails(formInitialDetails);
             setStatus({ success: true, message: 'Message sent successfully' });
@@ -66,9 +66,6 @@ export const Contact = () => {
                                 </Col>
                                 <Col sm={6} className="px-1">
                                     <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)}/>
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                                 </Col>
                                 <Col>
                                     <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
