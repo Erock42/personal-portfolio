e229eaf04baf31cdcf28b1d227465d37fa1fd59f
Erock42 <95666323+Erock42@users.noreply.github.com>
2024-12-10 22:17:44 -0500

    changed column size for email in form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,7 +64,7 @@ export const Contact = () => {
                                 <Col sm={6} className="px-1">
                                     <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                                 </Col>
-                                <Col sm={6} className="px-1">
+                                <Col sm={12} className="px-1">
                                     <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)}/>
                                 </Col>
                                 <Col>
