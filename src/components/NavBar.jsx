ba1b59abd6ca02efd0cbd7d94d09f555de45af1e
Erock42 <95666323+Erock42@users.noreply.github.com>
2026-01-13 21:51:08 -0500

    updated logo ; needs to be bigger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/img/EELogo.png";
+import logo from "../assets/img/EricEncodedLogo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
